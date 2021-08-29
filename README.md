bd909e01beb5076e396d6f47412e226d133d8eea
barbecue2021 <h19130058083@gmail.com>
2021-08-29 16:30:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+/*
+ * @Author: 距离 
+ * @Date: 2021-08-29 15:40:39 
+ * @Last Modified by: 距离
+ * @Last Modified time: 2021-08-29 16:22:36
+ */
+刘文Octopart Restful API项目
+
+
---
+++ index.js
@@ -0,0 +1,39 @@
+
+/**
+* @Author: 距离
+* @Date:   2021-08-29 15:17:06
+ * @Last Modified by: 距离
+ * @Last Modified time: 2021-08-29 16:22:36
+**/
+
+const { ApolloServer, gql } = require('apollo-server');
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
+
+
+/**
+ * 测试啊啊啊啊啊
+ * 156485456154
+ */
